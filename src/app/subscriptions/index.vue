3024828dd5a774046c0b8c626d7157ac13cf4015
George Mario <c.georgeiwu@gmail.com>
2018-09-03 09:19:37 +0100

    fixing new

--- src/app/subscriptions/index.vue
+++ src/app/subscriptions/index.vue
@@ -1,229 +1,77 @@
 <template>
-	<vue-scroll class="page-calendar">
-		<div id="cal" class="calendar-wrap card-base card-shadow--medium"></div>
-		
-		<el-dialog title="Add event" :visible.sync="dialogFormVisible">
-			<el-form :model="form" ref="form" label-position="top">
-				<el-col :span="13">
-					<el-form-item label="Title" prop="title" :rules="[ { required: true, message: 'Please input event title', trigger: 'blur' } ]">
-						<el-input v-model.trim="form.title"></el-input>
-					</el-form-item>
-				</el-col>
-				<el-col class="text-center" :span="1">&nbsp;</el-col>
-				<el-col :span="5">
-					<el-form-item label="All day">
-						<el-switch v-model="form.allDay"></el-switch>
-					</el-form-item>
-				</el-col>
-				<el-col :span="5">
-					<el-form-item label="Color">
-						<el-color-picker v-model="form.color" :predefine="predefineColors"></el-color-picker>
-					</el-form-item>
-				</el-col>
-				<el-form-item label="Description">
-					<el-input type="textarea" autosize v-model="form.description"></el-input>
-				</el-form-item>
-				<el-form-item label="Start" prop="startDate" :rules="[ { required: true, message: 'Please input a valid date', trigger: 'blur' } ]">
-					<el-col :span="11">
-						<el-date-picker type="date" placeholder="Pick a date" v-model="form.startDate" style="width: 100%;"></el-date-picker>
-					</el-col>
-					<el-col v-if="!form.allDay" class="text-center" :span="2">-</el-col>
-					<el-col v-if="!form.allDay" :span="11">
-						<el-time-picker type="fixed-time" placeholder="Pick a time" v-model="form.startTime" style="width: 100%;" format="HH:mm"></el-time-picker>
-					</el-col>
-				</el-form-item>
-				<el-form-item label="End">
-					<el-col :span="11">
-						<el-date-picker type="date" placeholder="Pick a date" v-model="form.endDate" style="width: 100%;"></el-date-picker>
-					</el-col>
-					<el-col v-if="!form.allDay" class="text-center" :span="2">-</el-col>
-					<el-col v-if="!form.allDay" :span="11">
-						<el-time-picker type="fixed-time" placeholder="Pick a time" v-model="form.endTime" style="width: 100%;" format="HH:mm"></el-time-picker>
-					</el-col>
-				</el-form-item>
-			</el-form>
-			<span slot="footer" class="dialog-footer">
-				<el-button @click="closeEventDialog">Cancel</el-button>
-				<el-button type="primary" @click="setEvent">Save</el-button>
-			</span>
-		</el-dialog>
-	</vue-scroll>
+	<div class="page-table ">
+		<div class="page-header card-base card-shadow--small" style="padding-top: 10px; padding-bottom: 10px; margin-bottom: 10px ">
+			<custom-filter first-button-name="Add" second-button-name="Download" />	
+		</div>
 
+		<custom-table :table-data="tableData" />
+	</div>
 </template>
 
 <script>
-import moment from 'moment-timezone'
-import $ from 'jquery'
-import 'fullcalendar'
-import 'fullcalendar/dist/fullcalendar.css'
+
+import CustomTable from '@/components/CustomTable.vue'
+import CustomFilter from '@/components/CustomFilter.vue'
 
 export default {
-	name: 'Calendar',
+	name: 'TableElement',
+	components: {
+		CustomTable,
+		CustomFilter
+	},
 	data() {
-		const __Y = moment().format('YYYY')
-		const __M = moment().format('MM')
-
-		return {
-			cal: null,
-			dialogFormVisible: false,
-			form: {
-				title: '',
-				description: '',
-				allDay: false,
-				startDate: '',
-				startTime: '',
-				endDate: '',
-				endTime: '',
-				color: '#4a596a'
-			},
-			predefineColors: [
-				'#ff4500',
-				'#ff8c00',
-				'#ffd700',
-				'#90ee90',
-				'#00ced1',
-				'#1e90ff',
-				'#4a596a',
-				'#c71585'
-			],
-			options: {
-				customButtons: {
-					addEvent: {
-						text: '✚',
-						click: this.addEventDialog
-					}
-				},
-				lang: 'en',
-				header: {
-					left:   'prev,next today',
-					center: 'title',
-					right:  'month,agendaWeek,agendaDay,listWeek addEvent'
-				},
-				height: "auto",
-				firstDay: 1,
-				allDaySlot: true,
-				slotEventOverlap: true,
-				selectable: true,
-				selectHelper: true,
-				timeFormat: 'HH:mm',
-				navLinks: true, // can click day/week names to navigate views
-				editable: true,
-				eventLimit: true, // allow "more" link when too many events
-				events: [
-					{ title: 'All Day Event', start: __Y+'-'+__M+'-01' },
-					{ title: 'Long Event', start: __Y+'-'+__M+'-07', end: __Y+'-'+__M+'-10' },
-					{ id: 999, title: 'Repeating Event', start: __Y+'-'+__M+'-09T16:00:00' },
-					{ id: 999, title: 'Repeating Event', start: __Y+'-'+__M+'-16T16:00:00' },
-					{ title: 'Conference', start: __Y+'-'+__M+'-11', end: __Y+'-'+__M+'-13' },
-					{ title: 'Meeting', start: __Y+'-'+__M+'-12T10:30:00', end: __Y+'-'+__M+'-12T12:30:00' },
-					{ title: 'Lunch', start: __Y+'-'+__M+'-12T12:00:00' },
-					{ title: 'Meeting', start: __Y+'-'+__M+'-12T14:30:00' },
-					{ title: 'Happy Hour', start: __Y+'-'+__M+'-12T17:30:00' },
-					{ title: 'Dinner', start: __Y+'-'+__M+'-12T20:00:00' },
-					{ title: 'Birthday Party', start: __Y+'-'+__M+'-13T07:00:00' },
-					{ title: 'Click for Google', url: 'http://google.com/', start: __Y+'-'+__M+'-28' }
-				],
-				dayClick: this.dayClick,
-				eventClick: this.eventClick,
-				select: this.select
-			}
-		}
-	},
-	methods: {
-		closeEventDialog() {
-			this.$refs.form.resetFields()
-			this.dialogFormVisible = false
-			this.form = {
-				title: '',
-				description: '',
-				allDay: false,
-				startDate: '',
-				startTime: '',
-				endDate: '',
-				endTime: '',
-				color: '#4a596a'
-			}
+      	return {
+        	tableData: [
+				{ date: '2016-05-03', name: 'Tom', address: 'No. 189, Grove St, Los Angeles', tag: 'Home' },
+				{ date: '2016-05-02', name: 'Tom', address: 'No. 189, Grove St, Los Angeles', tag: 'Office' },
+				{ date: '2016-05-04', name: 'Tom', address: 'No. 189, Grove St, Los Angeles', tag: 'Home' },
+				{ date: '2016-05-01', name: 'Tom', address: 'No. 189, Grove St, Los Angeles', tag: 'Office' },
+				{ date: '2016-05-01', name: 'Tom', address: 'No. 189, Grove St, Los Angeles', tag: 'Office' },
+				{ date: '2016-05-01', name: 'Tom', address: 'No. 189, Grove St, Los Angeles', tag: 'Office' },
+				{ date: '2016-05-01', name: 'Tom', address: 'No. 189, Grove St, Los Angeles', tag: 'Office' },
+				{ date: '2016-05-01', name: 'Tom', address: 'No. 189, Grove St, Los Angeles', tag: 'Office' },
+				{ date: '2016-05-01', name: 'Tom', address: 'No. 189, Grove St, Los Angeles', tag: 'Office' },
+				{ date: '2016-05-03', name: 'Tom', address: 'No. 189, Grove St, Los Angeles', tag: 'Home' },
+				{ date: '2016-05-02', name: 'Tom', address: 'No. 189, Grove St, Los Angeles', tag: 'Office' },
+				{ date: '2016-05-04', name: 'Tom', address: 'No. 189, Grove St, Los Angeles', tag: 'Home' },
+				{ date: '2016-05-01', name: 'Tom', address: 'No. 189, Grove St, Los Angeles', tag: 'Office' },
+				{ date: '2016-05-01', name: 'Tom', address: 'No. 189, Grove St, Los Angeles', tag: 'Office' },
+				{ date: '2016-05-01', name: 'Tom', address: 'No. 189, Grove St, Los Angeles', tag: 'Office' },
+				{ date: '2016-05-01', name: 'Tom', address: 'No. 189, Grove St, Los Angeles', tag: 'Office' },
+				{ date: '2016-05-01', name: 'Tom', address: 'No. 189, Grove St, Los Angeles', tag: 'Office' },
+				{ date: '2016-05-01', name: 'Tom', address: 'No. 189, Grove St, Los Angeles', tag: 'Office' }
+			]
+      	}
+    },
+    methods: {
+		formatter(row, column) {
+        	return row.address;
 		},
-		setEvent() {
-			this.$refs.form.validate((valid) => {
-				if (valid) {
-
-					let start = moment(this.form.startDate)
-					if(this.form.startTime) {
-						start.add(moment(this.form.startTime).format('HH'), 'hours')
-						start.add(moment(this.form.startTime).format('mm'), 'minutes')
-					}
-
-
-					let event = {
-						id: new Date().getTime(), 
-						title: this.form.title, 
-						description: this.form.description, 
-						start:  start,
-						allDay: this.form.allDay
-					}
-
-					if(this.form.color) event.color = this.form.color
-					if(this.form.endDate) {
-						let end = moment(this.form.endDate)
-						if(this.form.endTime) {
-							end.add(moment(this.form.endTime).format('HH'), 'hours')
-							end.add(moment(this.form.endTime).format('mm'), 'minutes')
-						} else {
-							end.add(24, 'hours')
-						} 
-
-						event.end = end
-					}
-
-					this.cal.fullCalendar('renderEvent', event, true);
-
-					this.closeEventDialog()
-				} else {
-					return false;
-				}
-			});
+		filterTag(value, row) {
+        	return row.tag === value;
 		},
-		addEventDialog() {
-			this.dialogFormVisible = true
-		},
-		dayClick(date, event, view) {
-			this.form.startDate = date
-			this.dialogFormVisible = true
-		},
-		eventClick(calEvent, jsEvent, view) {
-			this.form = {
-				title: calEvent.title,
-				description: calEvent.description,
-				allDay: calEvent.allDay,
-				startDate: calEvent.start,
-				startTime: calEvent.start,
-				endDate: calEvent.end,
-				endTime: calEvent.end,
-				color: calEvent.color || '#4a596a'
-			}
-			this.dialogFormVisible = true
-		},
-		select(start, end) {
-			this.form.startDate = start
-			this.form.endDate = end
-			this.dialogFormVisible = true
-		},
-	},
-	mounted() {
-		this.cal = $(window.document.getElementById('cal'))
-		this.cal.fullCalendar(this.options)
-	}
+		filterHandler(value, row, column) {
+        	const property = column['property'];
+        	return row[property] === value;
+      	}
+    }
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import '../../assets/scss/_variables';
 
-/*.page-calendar {
-	.calendar-wrap {
-		//background: white;
-	}
-}*/
+.table-box {
+	overflow: auto;
+}
+
+.pagination {
+	height: 55px; 
+	text-align: right;
+	background-color: rgba(243,247,250,.5);
+	border-top-left-radius: 0px;
+	border-top-right-radius: 0px;
+}
+
 </style>
 
